4719b16164fb31f111b52e0f841da23ffc9f1447
Edmundo Sanchez <zomundo@gmail.com>
2018-11-22 17:29:10 -0600

    implemented option  --report
    -r now parses start and end date and queries activity watch

--- add-activity.py
+++ add-activity.py
@@ -16,6 +16,21 @@ arg_parser.add_argument('-r','--report',help='Show activity report',action='stor
 args = arg_parser.parse_args()
 
 # parse time differently if report was requested
+if args.report:
+    #start time defaukts to
+    if not args.start:
+        args.start = datetime.now(timezone(timedelta(hours=-6)))
+        args.start = args.start.replace(hour=0, minute=0, second=0, microsecond=0)
+    else:
+        default_timedate = datetime.now(timezone(timedelta(hours=-6)))
+        default_timedate = default_timedate.replace(hour=0, minute=0, second=0, microsecond=0)
+        args.start = parser.parse(args.start,default=default_timedate)
+    # if end time not set, use now.
+    if not args.end:
+        args.end = datetime.now(timezone(timedelta(hours=-6)))
+    aw_help.bucket_report(args.bucket,args.start,args.end)
+    exit()
+
 # If sart_time set,
 if args.start:
     # parse the string
